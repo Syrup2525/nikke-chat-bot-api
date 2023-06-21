15e1f5fae778a4bc2aa4553a23308db3ecb371d6
Syrup2525 <srls8786@gmail.com>
2023-06-21 16:10:33 +0900

    명령어 추가

--- api/middleware/command.js
+++ api/middleware/command.js
@@ -1,5 +1,6 @@
 const { Request, Response } = require('express')
 const { isBlank } = require('../../util/commonUtil.js')
+const { commandList, helpMessage} = require('../../string.js')
 
 /**
  * 
@@ -19,6 +20,26 @@ const run = async (req, res) => {
         return res.status(400).send({
             message: "command 값의 형태는 string 입니다."
         })
+    }
+
+    const commandSplit = command.split(" ")
+
+    // 명령어 인자가 1개인 경우
+    if (commandSplit.length === 1) {
+        const command = commandSplit[0]
+
+        switch (command) {
+            // 명령어
+            case commandList.help:
+                return res.send({
+                    code: 0,
+                    message: "success",
+                    result: helpMessage,
+                })
+
+            default:
+                break
+        }
     }
 
     res.send({
---
+++ string.js
@@ -0,0 +1,26 @@
+const helpMessage = () => {
+    let message = ""
+    message += "< 사용 가능한 명령어 리스트 >\n"
+    message += "\n"
+    message += "(~˘▾˘)~♫•*¨*•.¸¸♪\n"
+    message += "\n"
+    message += "사용 방법 \n"
+    message += "- 띄어쓰기에 유의하여 주세요. \n"
+    message += "\n"
+    message += "(~˘▾˘)~♫•*¨*•.¸¸♪ \n"
+    message += "\n"
+    message += "일반뽑기 시물레이션 \n"
+    message += "=일반뽑기 (또는 =뽑) \n"
+    message += "\n"
+    message += "픽업뽁기 시뮬레이션 \n"
+    message += "=픽업뽑기 (또는 =픽) \n"
+
+    return message
+}
+
+module.exports = {
+    commandList: {
+        help: "명령어",
+    },
+    helpMessage: helpMessage(),
+}
