111dbfc999c11d5a771e53ad5f82c17aa207968c
Syrup2525 <srls8786@gmail.com>
2023-06-22 11:48:24 +0900

    픽업 뽑기 구현

--- api/middleware/command.js
+++ api/middleware/command.js
@@ -30,18 +30,73 @@ const run = async (req, res) => {
     if (commandSplit.length === 1) {
         const command = commandSplit[0]
 
-        if (commandList.help.includes(command)) {
+        if (commandList.help.commands.includes(command)) {
             return res.send({
                 code: 0,
                 message: "success",
                 result: helpMessage,
             })
-        } else if (commandList.normalGacha.includes(command)) {
+        } else if (command == "홍련") {
+            let count = 0
+
+            let r = 0
+            let sr = 0
+            let ssr = 0
+            let pilgrim = 0
+
+            while (true) {
+                count += 1
+
+                const item = gacha(false)
+
+                switch (item.rarity) {
+                    case "R":
+                        r += 1
+                        break
+
+                    case "SR":
+                        sr += 1
+                        break
+
+                    case "SSR":
+                        ssr += 1
+                        break
+
+                    default:
+                        break
+                }
+
+                if (item.nikke.company === "PILGRIM") {
+                    pilgrim += 1
+                }
+
+                if (item.nikke.name === "홍련") {
+                    break
+                }
+            }
+
+            let message = ""
+            message += `지휘관은 홍련이 ${count.toLocaleString()} 뽑기 만에 나왔네요\n`
+            message += "\n" 
+            message += `R: ${r.toLocaleString()}\n`
+            message += `SR: ${sr.toLocaleString()}\n`
+            message += `SSR: ${ssr.toLocaleString()}\n`
+            message += `PILGRIM: ${pilgrim.toLocaleString()}`
+
             return res.send({
                 code: 0,
                 message: "success",
-                result: normalGacha(),
+                result: message,
             })
+        } else if (commandList.normalGacha.commands.includes(command)) {
+            return executeGacha(res, false, null)
+        } else {
+            // 픽업 가챠 확인
+            for (const pickUpGacha of commandList.pickUpGacha.list) {
+                if (pickUpGacha.commands.includes(command)) {
+                    return executeGacha(res, true, pickUpGacha.nikke)
+                }
+            }
         }
     }
 
@@ -51,31 +106,111 @@ const run = async (req, res) => {
     })
 }
 
-const normalGacha = () => {
-    let number = getRandomNumber(1, 100)
+/**
+ * 10연 뽑기 실행 후 res.send 실행
+ * @param {Response} res express response 객체 
+ * @param {boolean} isPickUp 픽업 뽑기 여부
+ * @param {Object} pickUpNikke 픽업 대상 니케
+ * @returns 결과 문자열
+ */
+const executeGacha = (res, isPickUp, pickUpNikke) => {
+    const items = []
 
-    let rarity = []
+    for (let i = 0; i < 10; i++) {
+        items.push(gacha(isPickUp, pickUpNikke))
+    }
+
+    res.send({
+        code: 0,
+        message: "success",
+        result: getGachaMessage(items),
+    })
+}
+
+/**
+ * 메시지 문자열 생성
+ * @param {Array} items 니케 배열
+ */
+const getGachaMessage = (items) => {
+    let message = ""
+
+    for (const item of items) {
+        switch (item.rarity) {
+            case "SSR":
+                message += "★"
+                break
+
+            case "SR":
+                message += "☆"
+                break
+
+            case "R":
+                message += "○"
+                break
+
+            default:
+                break
+        }
+    }
+
+    message += "\n\n"
+
+    for (const item of items) {
+        message += `[${item.rarity}] [${item.nikke.company}] ${item.nikke.name} \n`
+    }
+
+    return message.trim()
+}
+
+/**
+ * 단발 뽑기
+ * @param {boolean} isPickUp 픽업 뽑기 여부
+ * @param {Object} pickUpNikke 픽업 대상 니케
+ * @param {string} pickUpNikke.company 기업이름
+ * @param {string} pickUpNikke.name 니케이름
+ * @returns 
+ */
+const gacha = (isPickUp, pickUpNikke) => {
+    let number = getRandomNumber(1, 1000)
+
+    let rarity = ""
+    let nikkes = []
 
     // 4% SSR
-    if (number <= 4) {
-        number = getRandomNumber(1, 1000)
+    if (number <= 40) {
+        if (isPickUp) {
+            // 2% 픽업
+            if (number <= 20) {
+                return {
+                    rarity: "SSR",
+                    nikke: pickUpNikke,
+                }
+            }
+        }
 
         // 0.5% 필그림
         if (number <= 5) {
-            rarity = nikke.PILGRIM
-        // 99.5% SSR
+            rarity = "SSR"
+            nikkes = nikke.PILGRIM
+        // 3.5% SSR
         } else {
-            rarity = nikke.SSR
+            rarity = "SSR"
+            nikkes = nikke.SSR
         }
     // 43% SR
-    } else if (number <= 47) {
-        rarity = nikke.SR
+    } else if (number <= 470) {
+        rarity = "SR"
+        nikkes = nikke.SR
     // 53% R
     } else {
-        rarity = nikke.R 
+        rarity = "R"
+        nikkes = nikke.R 
     }
 
-    return rarity[getRandomNumber(0, rarity.length - 1)]
+    return {
+        rarity: rarity,
+        nikke: nikkes[getRandomNumber(0, nikkes.length - 1)],
+    }
 }
 
 module.exports = {
--- string.js
+++ string.js
@@ -1,27 +1,49 @@
+const commandList = {
+    help: {
+        commands: ["명령어"],
+    },
+    normalGacha: {
+        type: "normalGacha",
+        description: "일반 10명 모집 시뮬레이션\n=일반뽑기 (또는 =뽑)",
+        commands: ["일반뽑기", "일뽑", "일", "뽑"],
+    },
+    pickUpGacha: {
+        type: "pickUpGacha",
+        description: "픽업 10명 모집 시뮬레이션\n[메어리 : 베이 갓데스] =수어리\n[네온 : 블루 오션] =수네온",
+        list: [
+            {
+                commands: ["수어리"],
+                nikke: {
+                    company: "TETRA",
+                    name: "메어리 : 베이 갓데스",
+                },
+            },
+            {
+                commands: ["수네온"],
+                nikke: {
+                    company: "ELYSION",
+                    name: "네온 : 블루 오션",
+                },
+            }
+        ]
+    }
+}
+
 const helpMessage = () => {
     let message = ""
     message += "< 사용 가능한 명령어 리스트 >\n"
     message += "\n"
-    message += "(~˘▾˘)~♫•*¨*•.¸¸♪\n"
-    message += "\n"
     message += "사용 방법 \n"
     message += "- 띄어쓰기에 유의하여 주세요. \n"
     message += "\n"
-    message += "(~˘▾˘)~♫•*¨*•.¸¸♪ \n"
+    message += `▶ ${commandList.normalGacha.description} \n`
     message += "\n"
-    message += "일반뽑기 시물레이션 \n"
-    message += "=일반뽑기 (또는 =뽑) \n"
-    message += "\n"
-    message += "픽업뽁기 시뮬레이션 \n"
-    message += "=픽업뽑기 (또는 =픽) \n"
+    message += `▶ ${commandList.pickUpGacha.description}`
 
     return message
 }
 
 module.exports = {
-    commandList: {
-        help: ["명령어"],
-        normalGacha: ["일반뽑기", "뽑"]
-    },
+    commandList: commandList,
     helpMessage: helpMessage(),
 }
